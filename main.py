6d819de9ff6708e1c7e92d84a69c7da1d5cfddbd
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-04-20 13:19:52 +0300

    Excercises from 1 to 3 acomplished

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import requests
+
+
+languages = ['Python', 'Java', 'Javascript', 'C++', 'Pascal']
+vacancies = {}
+for lang in languages:
+    payload = {
+        'text': lang,
+        'area': '1',
+        'period': '30'
+    }
+    response = requests.get('https://api.hh.ru/vacancies', params=payload)
+    vacancies[lang] = response.json()['found']
+print(vacancies)
